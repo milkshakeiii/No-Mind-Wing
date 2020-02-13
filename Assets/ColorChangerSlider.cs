24ac067a5077fe8728d3fe55a24e980c51465b47
Henry Solberg <milkshakeiii@gmail.com>
2020-02-13 10:07:10 -0800

    pixelgrid and color stuff

---
+++ Assets/ColorChangerSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangerSlider : MonoBehaviour
+{
+    public bool red = false;
+    public bool green = false;
+    public bool blue = false;
+
+    public UnityEngine.UI.Image targetImage;
+
+    public void OnValueChanged(float value)
+    {
+        if (red)
+            targetImage.color = new Color(value, targetImage.color.g, targetImage.color.b);
+        if (green)
+            targetImage.color = new Color(targetImage.color.r, value, targetImage.color.b);
+        if (blue)
+            targetImage.color = new Color(targetImage.color.r, targetImage.color.g, value);
+    }
+}
---
+++ Assets/PixelGridManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelGridManager : MonoBehaviour
+{
+    private UnityEngine.UI.Button[,] buttonGrid = new UnityEngine.UI.Button[0, 0];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPixelSize();
+
+        SpawnButtons(10);
+    }
+
+    private void SetPixelSize()
+    {
+        int imageEndSize = SpriteManager.SPRITE_SIZE;
+
+        Vector2 pixelCenter = new Vector2(Screen.width / 4, Screen.height / 2);
+
+        int halfSize = imageEndSize / 2;
+        int wideMultiplesAvailable = Mathf.FloorToInt((float)pixelCenter.x / (float)halfSize);
+        int tallMultiplesAvailable = Mathf.FloorToInt((float)pixelCenter.y / (float)halfSize);
+        int multiplesAvailable = Mathf.Min(wideMultiplesAvailable, tallMultiplesAvailable);
+        if (multiplesAvailable == 0)
+        {
+            throw new UnityException("It looks like there aren't " + imageEndSize.ToString() +
+                                     " pixels available.  I only have " + pixelCenter.x.ToString() + ".");
+        }
+        int rectTransformSize = imageEndSize * multiplesAvailable;
+        gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(rectTransformSize, rectTransformSize);
+    }
+
+    private void SpawnButtons(int buttonSize)
+    {
+        if (SpriteManager.SPRITE_SIZE % buttonSize != 0)
+        {
+            throw new UnityException(buttonSize.ToString() + " does not divide " + SpriteManager.SPRITE_SIZE + "!");
+        }
+        for (int i = 0; i < buttonGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < buttonGrid.GetLength(1); j++)
+            {
+                UnityEngine.UI.Button button = buttonGrid[i, j];
+                Destroy(button);
+            }
+        }
+        int buttonGridSize = SpriteManager.SPRITE_SIZE / buttonSize;
+        buttonGrid = new UnityEngine.UI.Button[buttonGridSize, buttonGridSize];
+        for (int i = 0; i < buttonGrid.GetLength(0); i++)
+        {
+            for (int j = 0; j < buttonGrid.GetLength(1); j++)
+            {
+                GameObject buttonObject = new GameObject("Button " + i.ToString() + j.ToString());
+                UnityEngine.UI.Button buttonComponent = buttonObject.AddComponent<UnityEngine.UI.Button>();
+                RectTransform rectTransform = buttonObject.AddComponent<RectTransform>();
+                buttonObject.transform.SetParent(gameObject.transform);
+                rectTransform.anchorMin = new Vector2((float)i / buttonGridSize, (float)j / buttonGridSize);
+                rectTransform.anchorMax = new Vector2((float)(i+1) / buttonGridSize, (float)(j+1) / buttonGridSize);
+                rectTransform.offsetMin = Vector2.zero;
+                rectTransform.offsetMax = Vector2.zero;
+                UnityEngine.UI.Image image = buttonObject.AddComponent<UnityEngine.UI.Image>();
+                image.sprite = SpriteManager.Instance().SpriteFromName("square");
+                //buttonScript.targetGraphic = image;
+                PixelGridButton buttonScript = buttonObject.AddComponent<PixelGridButton>();
+                buttonComponent.onClick.AddListener(buttonScript.OnClicked);
+            }
+        }
+    }
+
+    private void ButtonClicked()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
