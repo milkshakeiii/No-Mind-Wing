f2abf749f6a2fa4dacfa58cd94a3cbf5ef628dd4
Henry Solberg <milkshakeiii@gmail.com>
2020-02-19 13:05:11 -0800

    blind coding keycode stuff

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -15,6 +15,45 @@ public class PlayerManager : MonoBehaviour
 
     private Dictionary<ResourceType, float> stockpiles = new Dictionary<ResourceType, float>();
 
+    private UnityEngine.Input.KeyCode[] engineKeyCodes = new UnityEngine.Input.KeyCode[9]
+    {
+        UnityEngine.Input.KeyCode.A,
+        UnityEngine.Input.KeyCode.S,
+        UnityEngine.Input.KeyCode.D,
+        UnityEngine.Input.KeyCode.W,
+        UnityEngine.Input.KeyCode.Q,
+        UnityEngine.Input.KeyCode.E,
+        UnityEngine.Input.KeyCode.Z,
+        UnityEngine.Input.KeyCode.X,
+        UnityEngine.Input.KeyCode.C,
+    }
+
+    private UnityEngine.Input.KeyCode[] launcherKeyCodes = new UnityEngine.Input.KeyCode[9]
+    {
+        UnityEngine.Input.KeyCode.Spacebar,
+        UnityEngine.Input.KeyCode.F,
+        UnityEngine.Input.KeyCode.G,
+        UnityEngine.Input.KeyCode.V,
+        UnityEngine.Input.KeyCode.B,
+        UnityEngine.Input.KeyCode.R,
+        UnityEngine.Input.KeyCode.T,
+        UnityEngine.Input.KeyCode.Control,
+        UnityEngine.Input.KeyCode.Tab,
+    }
+
+    private static UnityEngine.Input.KeyCode[] bayKeyCodes = new UnityEngine.Input.KeyCode[9]
+    {
+        UnityEngine.Input.KeyCode.1,
+        UnityEngine.Input.KeyCode.2,
+        UnityEngine.Input.KeyCode.3,
+        UnityEngine.Input.KeyCode.4,
+        UnityEngine.Input.KeyCode.5,
+        UnityEngine.Input.KeyCode.6,
+        UnityEngine.Input.KeyCode.7,
+        UnityEngine.Input.KeyCode.8,
+        UnityEngine.Input.KeyCode.9,
+    }
+
     public void AddToTeam(string buildstring)
     {
         teamBuildStrings.Add(buildstring);
@@ -100,5 +139,38 @@ public class PlayerManager : MonoBehaviour
     public void RemoveFromSelection(Vessel vessel)
     {
         selection.Remove(vessel);
+    }
+
+    void Update ()
+    {
+	for (int i = 0; i < engineKeyCodes.Length; i++)
+        {
+            UnityEngine.Input.KeyCode keyCode = engineKeyCodes[i];
+            if (UnityEngine.Input.GetKeyDown(keyCode))
+            {
+                foreach (Vessel vessel in selection)
+                {
+                    vessel.IgniteEngines(new int[1]{i});
+                }
+            }
+            if (UnityEngine.Input.GetKeyUp(keyCode))
+            {
+                foreach (Vessel vessel in selection)
+                {
+                    vessel.QuenchEngines(new int[1]{i});
+                }
+            }
+        }
+	for (int i = 0; i < launcherKeyCodes.Length; i++)
+        {
+            UnityEngine.Input.KeyCode keyCode = engineKeyCodes[i];
+            if (UnityEngine.Input.GetKeyDown(keyCode))
+            {
+                foreach (Vessel vessel in selection)
+                {
+                    vessel.Fire(new int[1]{i});
+                }
+            }
+        }
     }
 }
